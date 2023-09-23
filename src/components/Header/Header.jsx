bec51cce4a983c14ea4653b9c68d9a75f35706d8
david crespo <davidcrespo@iMac-de-david.local>
2023-09-23 12:38:01 +0200

    Tienda To Do List

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,27 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <li>
+          <h3>Inicio</h3>
+        </li>
+        <li>
+          <h3>Nosotros</h3>
+        </li>
+        <li>
+          <img src="https://img.icons8.com/external-flaticons-flat-flat-icons/12x/external-aloe-vera-plants-flaticons-flat-flat-icons-2.png" alt="Logo cosmética" />
+        </li>
+        <li>
+          <h3>Tienda</h3>
+        </li>
+        <li>
+          <h3>To Do List</h3>
+        </li>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
